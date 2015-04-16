b594e9896a2f66fb7f5e1e94cbc1c18e05e931ac
Üllar Seerme <yates@protonmail.ch>
2015-04-16 21:27:31 +0300

    Added clean-up prompt and rewrote existing prompts

--- README.md
+++ README.md
@@ -5,4 +5,10 @@ I created this script to make it easier on myself when updating my Ghost blog in
 ### Changed
 - Write a check that returns latest version number before proceeding.
 - Add intermediary update if difference between current and latest version is too large.
+
+### [0.0.1] – 2015-04-16
+#### Added
 - Prompt for a clean-up of downloaded .zip file and/or backup.
+
+#### Changed
+- Rewrote prompts to be "while true" loops.
--- update.sh
+++ update.sh
@@ -2,26 +2,48 @@
 # Üllar Seerme
 # Script updates Ghost blogging platform to latest version.
 
-read -p "Enter the full path of the Ghost installation: " DIRECTORY
-if [ ! -d "$DIRECTORY" ]; then
-	echo "Path does not exist"
-	exit 1
-fi
+while true; do
+	read -p "Enter the full path of the Ghost installation directory: " DIRECTORY
+	if [ -d $DIRECTORY ]; then
+		break
+	else
+		echo "Path does not exist. Try again"
+	fi
+done
 
-read -p "Do you want a backup to be created via tar from $DIRECTORY? (y/n): " ANS
-if [ $ANS == "n" ]; then
-	echo "Continuing without backup"
-elif [ $ANS == "y" ]; then
-	DATE=$(date +%Y-%m-%d)
-	tar -cvf ghost-$DATE.tar.gz $DIRECTORY > /dev/null
-	echo "Created backup named ghost-$DATE.tar.gz"
+while true; do
+	read -p "Do you want a Tar backup to be created from $DIRECTORY? (y/n): " BK
+	if [ ${BK,,} == "n" ]; then
+		echo "Continuing without backup"
+		break
+	elif [ ${BK,,} == "y" ]; then
+		DATE=$(date +%Y-%m-%d)
+		tar -cvf ghost-backup-$DATE.tar.gz $DIRECTORY > /dev/null
+		echo "Created backup named ghost-backup-$DATE.tar.gz"
+		break
+	else
+		echo "Enter either "y" or "n""
+	fi
+done
+
+if [ ! -f "ghost-latest.zip" ]; then
+	wget -q http://ghost.org/zip/ghost-latest.zip
+	echo "Downloaded latest version to $(pwd)"
 else
-	echo "Enter either "y" or "n""
-	exit 1
+	while true; do
+		read -p "File ghost-latest.zip already exists. Download again? (y/n): " DL
+		if [ ${DL,,} == "n" ]; then
+			echo "Continuing with current ghost-latest.zip file"
+			break
+		elif [ ${DL,,} == "y" ]; then
+			wget -Nq http://ghost.org/zip/ghost-latest.zip
+			echo "Downloaded latest version to $(pwd)"
+			break
+		else
+			echo "Enter either "y" or "n""
+		fi
+	done
 fi
-
-wget -q http://ghost.org/zip/ghost-latest.zip
-echo "Downloaded latest version to $(pwd)"
 
 rm -rf ${DIRECTORY}core/ > /dev/null
 echo "Deleted core/ from $DIRECTORY to prepare for update"
@@ -34,3 +56,33 @@ echo "Installed new dependencies for Node.JS"
 
 pm2 restart ghost > /dev/null
 echo "Restarted Ghost. Refresh your browser"
+
+if [ -f "ghost-latest.zip" ]; then
+	while true; do
+		read -p "Do you want to clean up downloaded ghost-latest.zip? (y/n): " CLN
+		if [ ${CLN,,} = "n" ]; then
+			break
+		elif [ ${CLN,,} == "y" ]; then
+			rm -f ghost-latest.zip
+			echo "Deleted ghost-latest.zip from $(pwd)"
+			break
+		else
+			echo "Enter either "y" or "n""
+		fi
+	done
+fi
+
+if [ ${BK,,} == "y" ]; then
+	while true; do
+		read -p "Do you want to clean up newly created backup ghost-backup-$DATE.tar.gz? (y/n): " CLN
+		if [ ${CLN,,} = "n" ]; then
+			break
+		elif [ ${CLN,,} == "y" ]; then
+			rm -f ghost-backup-$DATE.tar.gz
+			echo "Deleted ghost-backup-$DATE.tar.gz from $(pwd)"
+			break
+		else
+			echo "Enter either "y" or "n""
+		fi
+	done
+fi
